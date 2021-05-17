61bfb884d9c9e62d441780475fb0cf5fab65eea9
Dmitriy <akselmega95@gmail.com>
2021-05-17 08:11:22 +0300

    not done

---
+++ frontend/Client/modal/modalAction.js
@@ -0,0 +1,7 @@
+
+
+const btnActionModal = document.getElementById('btn-action-modal');
+
+btnActionModal.addEventListener('click', () => {
+    document.getElementById("modal-action").classList.toggle("show-modal-fields");
+})
